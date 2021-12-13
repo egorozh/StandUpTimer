f7d237b0ce302b35653ea981ee54ffb64c1cb754
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-12-13 18:00:10 +0300

    add day enum

--- StandUpTimer/Models/TimerSettings.cs
+++ StandUpTimer/Models/TimerSettings.cs
@@ -5,17 +5,23 @@ namespace StandUpTimer.Models;
 
 internal class TimerSettings : ViewModelBase
 {
-    public bool IsSunday { get; set; }
-    public bool IsMonday { get; set; } = true;
-    public bool IsTuesday { get; set; } = true;
-    public bool IsWednesday { get; set; } = true;
-    public bool IsThursday { get; set; } = true;
-    public bool IsFriday { get; set; } = true;
-    public bool IsSaturday { get; set; }
+    public Day Day { get; set; } = Day.Monday | Day.Tuesday | Day.Wednesday | Day.Thursday | Day.Friday;
 
     public TimeOnly FromTime { get; set; } = new(10, 0);
     public TimeOnly ToTime { get; set; } = new(18, 0);
 
     public TimeOnly EveryPeriod { get; set; }
     public TimeOnly StandTime { get; set; }
+}
+
+[Flags]
+internal enum Day
+{
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 4,
+    Thursday = 8,
+    Friday = 16,
+    Saturday = 32,
+    Sunday = 64
 }
--- StandUpTimer/ViewModels/MainWindowViewModel.cs
+++ StandUpTimer/ViewModels/MainWindowViewModel.cs
@@ -4,10 +4,31 @@ namespace StandUpTimer.ViewModels;
 
 internal class MainWindowViewModel : ViewModelBase
 {
+    public bool IsSunday { get; set; }
+    public bool IsMonday { get; set; }
+    public bool IsTuesday { get; set; }
+    public bool IsWednesday { get; set; }
+    public bool IsThursday { get; set; }
+    public bool IsFriday { get; set; }
+    public bool IsSaturday { get; set; }
+
     public TimerSettings TimerSettings { get; private set; }
 
     public MainWindowViewModel()
     {
         TimerSettings = new TimerSettings();
+
+        SetActiveDays(TimerSettings.Day);
+    }
+
+    private void SetActiveDays(Day day)
+    {
+        IsSunday = day.HasFlag(Day.Sunday);
+        IsMonday = day.HasFlag(Day.Monday);
+        IsTuesday = day.HasFlag(Day.Tuesday);
+        IsWednesday = day.HasFlag(Day.Wednesday);
+        IsThursday = day.HasFlag(Day.Thursday);
+        IsFriday = day.HasFlag(Day.Friday);
+        IsSaturday = day.HasFlag(Day.Saturday);
     }
 }
