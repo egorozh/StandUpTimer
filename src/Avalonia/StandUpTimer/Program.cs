6b174ffcd18f29c973a2d46293944d501f08c8e5
Egor Zheludkov <zheludkov.egor@mail.ru>
2022-01-01 23:38:03 +0300

    Make Mica actually working

--- src/Avalonia/StandUpTimer/Program.cs
+++ src/Avalonia/StandUpTimer/Program.cs
@@ -5,16 +5,16 @@ namespace StandUpTimer;
 
 internal class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
-
-    // Avalonia configuration, don't remove; also used by visual designer.
+    
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
+            .With(new Win32PlatformOptions
+            {
+                UseWindowsUIComposition = true
+            })
             .LogToTrace();
 }
