152de6bf6e89b65b5afafb416717ce18074009ea
Egor Zheludkov <zheludkov@cmsplanet.ru>
2022-01-21 21:43:16 +0300

    remove uwp proj

--- src/Avalonia/StandUpTimer/Program.cs
+++ src/Avalonia/StandUpTimer/Program.cs
@@ -9,12 +9,11 @@ internal class Program
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
     
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .With(new Win32PlatformOptions
             {
-                UseWindowsUIComposition = true
+                UseWindowsUIComposition = true,
             })
             .LogToTrace();
 }
